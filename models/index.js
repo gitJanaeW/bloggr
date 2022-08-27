a686a930337a4665c90c438ff37dd980d1054ff2
janae.wel <janae.wel@gmail.com>
2022-08-27 10:26:30 -0400

    created model relationships

--- models/index.js
+++ models/index.js
@@ -3,4 +3,50 @@ const Post = require('./Post');
 const Like = require('./Like');
 const Comment = require('./Comment');
 
+// user & post relationship
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+// to see which users commented on a single post
+User.belongsToMany(Post, {
+    through: Like,
+    as: 'liked_posts',
+    foreignKey: 'user_id'
+});
+
+// to see which posts a single user commented on
+Post.belongsToMany(User, {
+    through: Like,
+    as: 'liked_posts',
+    foreignKey: 'post_id'
+});
+
+// post & user's relationship with like
+Post.hasMany(Like, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Like, {
+    foreignKey: 'post_id'
+});
+
+// comment relationship with user & post
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// comment & post relationship
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
 module.exports = {User, Post, Like, Comment};
