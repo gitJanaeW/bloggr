d9fb65bd059acc6132a11895c162d336713521da
janae.wel <janae.wel@gmail.com>
2022-08-29 15:38:06 -0400

    reorganizing server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({});
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
@@ -27,8 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
